afe476651e4ab26f5fe5277994a163339c620cb3
VoranReygetov <voran009@gmail.com>
2024-03-21 08:57:45 +0200

    Minor changes of code smell

--- LibraryProject/models.py
+++ LibraryProject/models.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import Boolean, DateTime, create_engine
-# from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 
